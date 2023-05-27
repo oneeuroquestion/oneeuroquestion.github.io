fbcf86afd4268d599bda8af67ef3c09aa007d14c
OneEuroQuestion <mike@oneeuroquestion.com>
2023-05-27 11:12:09 +0200

    _ -> 01111111

---
+++ innext/static/9SbOGbXW_sDzayFj8ORIL/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a38466253adbfddb.js"],"/404":["static/chunks/pages/404-bacb9caeeabe49ce.js"],"/_error":["static/chunks/pages/error-54de1933a164a1ff.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
